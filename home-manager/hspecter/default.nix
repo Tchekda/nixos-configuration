811ff009ef2f161888b1ef1a13b12c43e4e67a9a
David Tchekachev <contact@tchekda.fr>
2021-02-03 23:08:33 +0100

    Fixed lock and fish ip

--- home-manager/hspecter/default.nix
+++ home-manager/hspecter/default.nix
@@ -47,13 +47,27 @@ in
 
       Service = {
         ExecStart = ''
-          ${pkgs.xautolock}/bin/xautolock -detectsleep -time 15 \
+          ${pkgs.xautolock}/bin/xautolock -noclose -detectsleep -time 15 \
            -locker "${pkgs.i3lock-color}/bin/i3lock-color -ti ${screenlocker} \
            --clock --pass-media-keys --pass-screen-keys --pass-power-keys --pass-volume-keys" \
-           -notify 10 \
-           -notifier "${pkgs.libnotify}/bin/notify-send 'Locking in 10 seconds'"
+           -notifier "${pkgs.libnotify}/bin/notify-send 'Locking in 10 seconds'" -notify 10 \
+           -killer "/run/current-system/systemd/bin/systemctl hibernate" -killtime 30
         '';
         Restart = "on-failure";
+      };
+    };
+    xss-lock = {
+      Unit = {
+        Description = "xss-lock, session locker service";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Install = { WantedBy = [ "graphical-session.target" ]; };
+
+      Service = {
+        Environment = "XDG_SESSION_TYPE=x11";
+        ExecStart = "${pkgs.xss-lock}/bin/xss-lock -l -s \${XDG_SESSION_ID} -- ${pkgs.xautolock}/bin/xautolock -locknow";
       };
     };
   };
