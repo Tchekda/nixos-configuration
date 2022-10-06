6783ba4160b1c9c8bdae8d0ad27591f2c497206a
David Tchekachev <contact@tchekda.fr>
2022-10-06 00:09:01 +0000

    init: gretchen

---
+++ gretchen/configuration.nix
@@ -0,0 +1,345 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+let
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      <home-manager/nixos>
+      ../tchekda_user.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot = {
+    loader = {
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 3;
+      };
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
+
+  environment.systemPackages = with pkgs; [
+    acpi
+    git
+    gnome.seahorse
+    gnupg
+    hicolor-icon-theme
+    nano
+    ntfs3g
+    wget
+    wireguard-tools
+  ];
+
+  hardware = {
+
+    bluetooth = {
+      enable = true;
+      # hsphfpd.enable = true;
+      # package = oldstable.bluezFull; # https://github.com/NixOS/nixpkgs/issues/177311#issuecomment-1154236306
+      package = unstable.bluez5-experimental;
+      # powerOnBoot = false;
+      settings = {
+        # A2DP https://nixos.wiki/wiki/Bluetooth#Enabling_A2DP_Sink
+        General = {
+          Enable = "Source,Sink,Media,Socket";
+          Experimental = true;
+        };
+      };
+    };
+
+    cpu.amd.updateMicrocode = true;
+
+    enableAllFirmware = true; # For wifi : https://github.com/NixOS/nixos-hardware/issues/8
+    enableRedistributableFirmware = true;
+
+    firmware = with pkgs; [];
+
+    i2c.enable = true;
+
+    logitech.wireless = {
+      enable = true;
+      enableGraphical = true;
+    };
+
+    opengl = {
+      driSupport = true;
+      driSupport32Bit = true;
+      setLdLibraryPath = true;
+      extraPackages = with pkgs; [
+        rocm-opencl-icd
+        rocm-opencl-runtime
+      ];
+      extraPackages32 = [
+      ];
+    };
+
+    pulseaudio.enable = false;
+
+    sane = {
+      enable = true;
+      extraBackends = [ pkgs.sane-airscan ];
+    };
+  };
+
+  home-manager.users.tchekda = {
+    programs.home-manager.enable = true;
+    home = {
+      username = "tchekda";
+      homeDirectory = "/home/tchekda";
+      packages = [ pkgs.home-manager ];
+    };
+    imports = [ ../home-manager/gretchen/default.nix ];
+  };
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  location.provider = "geoclue2";
+  
+  nix = {
+    extraOptions = "experimental-features = nix-command flakes";
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 10d";
+    };
+    trustedUsers = [ "root" "tchekda" ];
+  };
+
+  nixpkgs.config = {
+    pulseaudio = true;
+    allowUnfree = true;
+  };
+
+  networking = {
+    enableIPv6 = true;
+
+    hostName = "gretchen"; # Define your hostname.
+
+    networkmanager = {
+      enable = true;
+      wifi.backend = "iwd";
+    };
+
+    useDHCP = lib.mkDefault true;
+
+    wireless.iwd.enable = true;
+  };
+
+  programs = {
+    dconf.enable = true;
+    ssh.startAgent = true;
+  };
+
+  security = {
+    polkit.enable = true;
+    pam.services = {
+      login.fprintAuth = true;
+      xscreensaver.fprintAuth = true;
+    };
+    rtkit.enable = true;
+  };
+
+
+  # Enable the X11 windowing system.
+  services = {
+    acpid.enable = true;
+
+    avahi = {
+      enable = true;
+      nssmdns = true; # Restart nscd in case stops working again
+      ipv4 = true;
+      ipv6 = true;
+      publish = {
+        enable = true;
+        userServices = true;
+        addresses = true;
+        workstation = true;
+      };
+    };
+
+    blueman.enable = true;
+
+    dbus.packages = [ pkgs.gcr ];
+
+    ddccontrol.enable = true;
+
+    fstrim.enable = true;
+
+    fwupd.enable = true;
+
+    geoclue2.enable = true;
+
+    gnome = {
+      at-spi2-core.enable = true;
+    };
+
+    logind = {
+      lidSwitch = "suspend-then-hibernate";
+      lidSwitchExternalPower = "ignore";
+      lidSwitchDocked = "ignore";
+      extraConfig = ''
+        HandlePowerKey=suspend-then-hibernate
+        HandleSuspendKey=ignore
+      '';
+    };
+
+    openssh.enable = true;
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      wireplumber.enable = false;
+      media-session = {
+        enable = true;
+        config.bluez-monitor.rules = [
+          # https://gist.github.com/iwantroca/90eb080ea130e232cbe0d48f3595e846
+          {
+            # Matches all cards
+            matches = [{ "device.name" = "~bluez_card.*"; }];
+            actions = {
+              "update-props" = {
+                "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+                "bluez5.auto-connect" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+                # automatically switch between HSP/HFP and A2DP profiles when an input stream is detected https://wiki.archlinux.org/title/PipeWire
+                "bluez5.autoswitch-profile" = false; # Because now internal mic works
+                # mSBC is not expected to work on all headset + adapter combinations.
+                "bluez5.msbc-support" = true;
+                # SBC-XQ is not expected to work on all headset + adapter combinations.
+                "bluez5.sbc-xq-support" = true;
+              };
+            };
+          }
+          {
+            matches = [
+              # Matches all sources
+              { "node.name" = "~bluez_input.*"; }
+              # Matches all outputs
+              { "node.name" = "~bluez_outpuft.*"; }
+            ];
+            actions = {
+              "node.pause-on-idle" = true;
+            };
+          }
+        ];
+      };
+    };
+
+    printing = {
+      enable = true;
+      drivers = [ pkgs.cnijfilter2 pkgs.gutenprint pkgs.hplipWithPlugin ];
+    };
+
+    redshift.enable = true;
+
+    tlp = {
+      enable = false; # Linux Advanced Power Management : Kills some usb controllers, will need to figure out a solution
+      settings = {
+        RADEON_DPM_PERF_LEVEL_ON_AC = "auto";
+        RADEON_DPM_PERF_LEVEL_ON_BAT = "auto";
+        TLP_DEBUG = "arg bat disk lock nm path pm ps rf run sysfs udev usb";
+      };
+    };
+
+    tzupdate.enable = true;
+
+    xserver = {
+      enable = true;
+
+      deviceSection = ''
+        Option "DRI" "3"
+        Option "TearFree" "true"
+      '';
+
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+
+      extraConfig = ''
+        Section "OutputClass"
+          Identifier "AMDgpu"
+          MatchDriver "amdgpu"
+          Driver "amdgpu"
+          Option "TearFree" "true"
+        EndSection
+      '';
+
+      inputClassSections = [
+        ''
+          Identifier "mouse accel"
+          Driver "libinput"
+          MatchIsPointer "on"
+          Option "AccelProfile" "flat"
+          Option "AccelSpeed" "0"
+        ''
+      ];
+
+      layout = "us";
+
+      libinput = {
+        enable = true;
+        touchpad = {
+          naturalScrolling = true;
+          accelProfile = "flat";
+          disableWhileTyping = true;
+        };
+      };
+
+      useGlamor = true;
+
+      videoDrivers = [ "amdgpu" ];
+
+      windowManager = {
+        i3 = {
+          enable = true;
+          package = pkgs.i3-gaps;
+          extraSessionCommands = ''
+            eval $(${pkgs.gnome3.gnome-keyring}/bin/gnome-keyring-daemon --daemonize)
+            export SSH_AUTH_SOCK
+          '';
+        };
+      };
+
+      xkbVariant = "altgr-intl";
+
+
+    };
+
+  };
+
+  sound.enable = true;
+
+  system.stateVersion = "22.05"; # Did you read the comment?
+
+  systemd = {
+    sleep.extraConfig = "HibernateDelaySec=30m";
+
+    services = {
+      # Disable autostarts
+      cups-browsed.wantedBy = lib.mkForce [ ];
+      NetworkManager-wait-online.enable = false;
+      # wg-quick-wg0.wantedBy = lib.mkForce [ ];
+    };
+
+    user.services.geoclue-agent.wantedBy = [ "network.target" ];
+  };
+
+  time.hardwareClockInLocalTime = true;
+
+  users.users = {
+    tchekda.extraGroups = [ "audio" "networkmanager" "lpadmin" "scanner" "lp" "video" "i2c" ];
+    root.shell = pkgs.fish;
+  };
+}
---
+++ home-manager/gretchen/default.nix
@@ -0,0 +1,119 @@
+{ pkgs, config, lib, environment, ... }:
+let
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+  screenlocker = builtins.fetchurl {
+    url = https://files.tchekda.fr/suits-wallpaper.jpg;
+    sha256 = "sha256:18i26c2szmsas9r962ndncikp2lzqljg9rr4v2szp03hfp2sah0q";
+  };
+in
+{
+  imports = [
+    ../home.nix
+    ../battery.nix
+    ../desktop-packages.nix
+    ../alacritty.nix
+    ../i3.nix
+    ../dunst.nix
+  ];
+
+  home = {
+    sessionPath = [
+      "/home/tchekda/.yarn/bin"
+    ];
+    sessionVariables = {
+      HTTPSTAT_SAVE_BODY = "false";
+    };
+  };
+
+  programs = {
+
+    home-manager.enable = true;
+
+    fish = {
+      shellAbbrs =
+        {
+          nrs = "sudo nixos-rebuild -I \"nixos-config=/home/tchekda/nixos-configuration/gretchen/configuration.nix\" switch";
+          hms = "home-manager -f /home/tchekda/nixos-configuration/home-manager/gretchen/default.nix switch -b backup";
+        };
+    };
+
+    ssh.extraOptionOverrides = {
+      "IdentityFile" = "~/.ssh/id_ecdsa_sk";
+    };
+
+    vscode = {
+      enable = true;
+      package = pkgs.vscode;
+    };
+  };
+
+  services = {
+
+    caffeine.enable = true;
+
+    # dropbox.enable = true;
+
+    gnome-keyring.enable = true;
+
+    gpg-agent = {
+      enable = true;
+      enableExtraSocket = true;
+      pinentryFlavor = "tty";
+    };
+
+    mpris-proxy.enable = true;
+
+    picom = {
+      enable = true;
+      vSync = true;
+      shadow = true;
+      shadowExclude = [ "window_type *= 'normal' && ! name ~= ''" ];
+      backend = "glx";
+      fade = true;
+      fadeDelta = 5;
+    };
+  };
+
+  systemd.user.services = {
+
+    # dropbox.Install.WantedBy = lib.mkForce [ ];
+
+    xautolock = {
+      Unit = {
+        Description = "xautolock, session locker service";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Install = { WantedBy = [ "graphical-session.target" ]; };
+
+      Service = {
+        ExecStart = ''
+          ${pkgs.xautolock}/bin/xautolock -noclose -detectsleep -time 60 \
+           -locker "${pkgs.i3lock-color}/bin/i3lock-color -ti ${screenlocker} \
+           --clock --pass-media-keys --pass-screen-keys --pass-power-keys --pass-volume-keys" \
+           -notifier "${pkgs.libnotify}/bin/notify-send 'Locking in 30 seconds'" -notify 30 \
+        '';
+        # -killer "/run/current-system/systemd/bin/systemctl hibernate" -killtime 15
+        Restart = "on-failure";
+      };
+    };
+
+    xss-lock = {
+      Unit = {
+        Description = "xss-lock, session locker service";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Install = { WantedBy = [ "graphical-session.target" ]; };
+
+      Service = {
+        Environment = "XDG_SESSION_TYPE=x11";
+        ExecStart = "${pkgs.xss-lock}/bin/xss-lock -l -s \${XDG_SESSION_ID} -- ${pkgs.xautolock}/bin/xautolock -locknow";
+      };
+    };
+  };
+
+
+}
