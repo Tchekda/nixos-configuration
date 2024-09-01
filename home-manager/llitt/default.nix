1ba8a66a8918995e39e54a7b6dd2bbd1dc9e94f1
David Tchekachev <contact@tchekda.fr>
2024-09-01 20:55:45 +0200

    feat(llitt): add rebuild fish abbrs

--- home-manager/llitt/default.nix
+++ home-manager/llitt/default.nix
@@ -3,4 +3,13 @@
 {
   imports = [ ../home.nix ];
 
+  programs = {
+    fish = {
+      shellAbbrs =
+        {
+          nrs = "sudo nixos-rebuild -I \"nixos-config=/home/tchekda/nixos-configuration/llitt/configuration.nix\" switch";
+          hms = "home-manager -f /home/tchekda/nixos-configuration/home-manager/llitt/default.nix switch -b backup";
+        };
+    };
+  };
 }
