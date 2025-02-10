d89160e2bf9c56ae30fe977c598e367cbf79b1b1
David Tchekachev <contact@tchekda.fr>
2025-02-11 00:47:53 +0100

    chore: use stable rtorrent release

--- mross/seedbox.nix
+++ mross/seedbox.nix
@@ -1,14 +1,36 @@
-{ pkgs, config, lib, ... }:
-let
-  unstable = import <nixos-unstable> { };
-in
 {
-  users.groups.media.members = [ "plex" "radarr" "sonarr" "deluge" "tchekda" "nginx" ];
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+in
+# unstable = import <nixos-unstable> { };
+{
+  users.groups.media.members = [
+    "plex"
+    "radarr"
+    "sonarr"
+    "deluge"
+    "tchekda"
+    "nginx"
+  ];
 
   networking.firewall = {
     # rTorrent incoming
-    allowedTCPPortRanges = [{ from = 40000; to = 59000; }];
-    allowedUDPPortRanges = [{ from = 40000; to = 59000; }];
+    allowedTCPPortRanges = [
+      {
+        from = 40000;
+        to = 59000;
+      }
+    ];
+    allowedUDPPortRanges = [
+      {
+        from = 40000;
+        to = 59000;
+      }
+    ];
     # DHT
     allowedUDPPorts = [ 6881 ];
   };
@@ -19,32 +41,39 @@ in
       openFirewall = true;
     };
 
-    sonarr.enable = true;
-    radarr.enable = true;
+    sonarr = {
+      enable = true;
+      group = "media";
+    };
+    radarr = {
+      enable = true;
+      group = "media";
+    };
 
     rtorrent = {
       enable = true;
-      package = unstable.rtorrent;
+      # package = unstable.rtorrent;
       dataPermissions = "0777";
       downloadDir = "/srv/downloads";
       group = "media";
       configText = ''
         dht.mode.set = auto
         dht.port.set = 6881
-        
+
         log.add_output = "debug", "log"
-        
+
         # network.http.proxy_address.set = socks5h://45.67.230.130:30289
         # network.port_random.set = no
         network.port_range.set = 40000-59000
-        
+
         protocol.encryption.set = allow_incoming,try_outgoing,enable_retry
         protocol.pex.set = 1
 
         schedule2 = scgi_permission,0,0,"execute.nothrow=chmod,777,/run/rtorrent/rpc.sock"
-        
+        schedule2 = data_permission,0,0,"execute.nothrow=chgrp,media,/srv/downloads"
+
         system.umask.set = 000
-        
+
         trackers.use_udp.set = 1
       '';
     };
@@ -69,4 +98,3 @@ in
     };
   };
 }
-
