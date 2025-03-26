c01d7c3abde701ddf7e609231d582b09d8494800
David Tchekachev <contact@tchekda.fr>
2025-03-27 00:42:58 +0100

    chore(lens): bump deps

--- home-manager/hspecter/lens.nix
+++ home-manager/hspecter/lens.nix
@@ -1,8 +1,14 @@
-{ lib, fetchurl, appimageTools, wrapGAppsHook, makeWrapper }:
+{
+  lib,
+  fetchurl,
+  appimageTools,
+  wrapGAppsHook,
+  makeWrapper,
+}:
 
 let
   pname = "lens";
-  version = "2024.11.131815";
+  version = "2025.2.141554";
   channel = "latest";
   # channel = "beta";
   build = "${version}-${channel}";
@@ -11,7 +17,7 @@ let
   src = fetchurl {
     url = "https://api.k8slens.dev/binaries/Lens-${build}.x86_64.AppImage";
     # url = "https://downloads.k8slens.dev/ide/Lens-${build}.x86_64.AppImage";
-    sha256 = "sha256-Aezibc00tq6Y4WEQoVGsVkzojdCUY2LMRtCyaPUV4dc=";
+    sha256 = "sha256-2FW5v1bpCmxkZrPa+X6ee90c/SUtCsalzBLWsm7Vekg=";
     name = "${pname}.AppImage";
   };
 
@@ -23,25 +29,27 @@ in
 appimageTools.wrapType2 {
   inherit name src;
 
-  extraInstallCommands =
-    ''
-      mv $out/bin/${name} $out/bin/${pname}
-      source "${makeWrapper}/nix-support/setup-hook"
-      wrapProgram $out/bin/${pname} \
-        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}"
-      install -m 444 -D ${appimageContents}/lens-desktop.desktop $out/share/applications/${pname}.desktop
-      install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/512x512/apps/lens-desktop.png \
-         $out/share/icons/hicolor/512x512/apps/${pname}.png
-      substituteInPlace $out/share/applications/${pname}.desktop \
-        --replace 'Icon=lens-desktop' 'Icon=${pname}' \
-        --replace 'Exec=AppRun' 'Exec=${pname}'
-    '';
+  extraInstallCommands = ''
+    mv $out/bin/${name} $out/bin/${pname}
+    source "${makeWrapper}/nix-support/setup-hook"
+    wrapProgram $out/bin/${pname} \
+      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}"
+    install -m 444 -D ${appimageContents}/lens-desktop.desktop $out/share/applications/${pname}.desktop
+    install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/512x512/apps/lens-desktop.png \
+       $out/share/icons/hicolor/512x512/apps/${pname}.png
+    substituteInPlace $out/share/applications/${pname}.desktop \
+      --replace 'Icon=lens-desktop' 'Icon=${pname}' \
+      --replace 'Exec=AppRun' 'Exec=${pname}'
+  '';
 
   meta = with lib; {
     description = "The Kubernetes IDE";
     homepage = "https://k8slens.dev/";
     license = licenses.mit;
-    maintainers = with maintainers; [ dbirks RossComputerGuy ];
+    maintainers = with maintainers; [
+      dbirks
+      RossComputerGuy
+    ];
     platforms = [ "x86_64-linux" ];
   };
 }
